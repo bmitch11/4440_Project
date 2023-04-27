9a18fa9eafd0930443d5f5dc90b71cc3a1a21854
Bryce McNealey <bmcnealey3@gatech.edu>
2023-04-27 13:51:27 -0400

    Verion before meal_cont added

--- 4440_Project/main.py
+++ 4440_Project/main.py
@@ -1,12 +1,25 @@
-from flask import Flask
+from flask import Flask,render_template,request
 # create flask object
-app = Flask(__name__)
+app = Flask(__name__, static_folder='templates', static_url_path='')
 app.debug = True
 # create home page #
 @app.route("/")
 def index():
-    return app.send_static_file("Home.html")
+    return render_template("Home.html")
 
+@app.route('/chosen', methods=['POST'])
+def form_submit():
+    selected_option = request.form.get('query')
+    if selected_option == 'meals':
+        return render_template('mealPlan.html')
+    elif selected_option == 'overview':
+        return render_template('overview.html')
+    elif selected_option == 'Adult':
+        return render_template('Adult.html')
+    elif selected_option == '':
+        return render_template('Adult.html')
+    else:
+        return render_template('fake.html')
 #----------------main funtion-----------------#
 if __name__ == "__main__":
     app.run()
